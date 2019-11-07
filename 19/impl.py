7f3b14da4ca4734b60012f08228a06cc03b583ae
Niclas Simmler <niclas.simmler@gmail.com>
2019-11-07 12:16:15 +0100

    Updated Weekday calculation

--- 19/impl.py
+++ 19/impl.py
@@ -16,8 +16,8 @@ class Solution_Math:
         self.y2, self.m2, self.d2 = split_date_string(date_two)
         self.epoch1 = int((convert_to_epoch(self.y1, self.m1, self.d1)).strftime('%s'))
         self.epoch2 = int((convert_to_epoch(self.y2, self.m2, self.d2)).strftime('%s'))
-        self.wd1N, self.wd1 = self.get_week_day(self.y1, self.m1, self.d1)
-        self.wd2N, self.wd2 = self.get_week_day(self.y2, self.m2, self.d2)
+        self.wd1N, self.wd1 = self.calculate_week_day(self.y1, self.m1, self.d1)
+        self.wd2N, self.wd2 = self.calculate_week_day(self.y2, self.m2, self.d2)
         self.days_diff = abs(self.epoch1 - self.epoch2) // Solution_Math.DAY_IN_SECONDS
         self.sundays_on_the_first = list(self.calculate_sundays())
 
@@ -36,28 +36,14 @@ class Solution_Math:
                     yield next_sunday
                 next_sunday += Solution_Math.DAY_IN_SECONDS * 7
 
-    def get_week_day(self, year, month, day):
-        offset = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
-        week   = ['Sunday', 
-                'Monday', 
-                'Tuesday', 
-                'Wednesday', 
-                'Thursday',  
-                'Friday', 
-                'Saturday']
-        afterFeb = 1
-        if month > 2: afterFeb = 0
-        aux = year - 1700 - afterFeb
-        # dayOfWeek for 1700/1/1 = 5, Friday
-        dayOfWeek  = 5
-        # partial sum of days betweem current date and 1700/1/1
-        dayOfWeek += (aux + afterFeb) * 365                  
-        # leap year correction    
-        dayOfWeek += aux / 4 - aux / 100 + (aux + 100) / 400     
-        # sum monthly and day offsets
-        dayOfWeek += offset[month - 1] + (day - 1)               
-        dayOfWeek %= 7
-        return int(dayOfWeek//1), week[int(dayOfWeek//1)]
+    def calculate_week_day(self, year, month, day):
+        # Zeller's congruence
+        q = day
+        m = [-1,13,14,3,4,5,6,7,8,9,10,11,12][month]
+        K = year % 100
+        J = year // 100
+        h = (q + floor((13 * (m+1))/5) + K + floor(K/4) + floor(J/4) - (5 * J)) % 7
+        return h, ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][h]
 
     def __str__(self):
         return 'Start Date: YYYY={0},MM={1},DD={2},WD={3}-{4},Epoch={10}\nEnd Date: YYYY={5},MM={6},DD={7},WD={8}-{9},Epoch={11}\nDays Difference={12}\nSundays Count={13}'.format(self.y1, self.m1, self.d1, self.wd1N, self.wd1, self.y2, self.m2, self.d2, self.wd2N, self.wd2, self.epoch1, self.epoch2, self.days_diff, self.get_sundays_on_the_first_count())
